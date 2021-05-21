c6144be2e35e04c7fedeabeb3a252b6f61e3d646
yjdong9697 <yjdong9697@yonsei.ac.kr>
2021-05-21 15:25:57 +0900

    2021.05.21

--- 혼자 공부하는 머신러닝/chapter4-2.py
+++ 혼자 공부하는 머신러닝/chapter4-2.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.sparse.construct import random
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDClassifier
